547a36d122ac1583b605f0fa93b2952df4b72ca4
Mwanambuyu Akamandisa <amwaka98@gmail.com>
2024-04-16 22:17:40 +0200

    first commit

---
+++ digital-clock/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DigitalClock from './DigitalClock'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DigitalClock />
+  </React.StrictMode>,
+)
